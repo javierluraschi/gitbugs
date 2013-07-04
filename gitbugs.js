08ce9a3d7adbe0c6719702be20e77554c8e14bdd
Javier Luraschi <javierluraschi@hotmail.com>
2013-07-03 19:57:08 -0700

    Improved GitBugs.js Formatting

--- gitbugs.js
+++ gitbugs.js
@@ -1,80 +1,80 @@
 var UTIL = (function (my) {
-	my.foreach = function (data, callback) {
-	for (var idxElement = 0; idxElement < data.length; idxElement++) {
-		callback(idxElement, data[idxElement]);
-	}
-	};
-	    
-	my.parallel = function (actions, finished) {
-	finishedCount = 0;
-	var results = [];
-    
-	UTIL.foreach (actions, function (i, action) {
-		action.action(function (result) {
-		results[i] = result;
-		finishedCount++;
-    
-		if (finishedCount == actions.length) {
-			finished(results);
-		}
-		}, action.data);
-	});
-	};
-	    
-	my.saveFile = function (type, data, fileName) {
-	var uri = 'data:' + type + ';charset=utf-8,' + data;
-    
-	var downloadLink = document.createElement("a");
-	downloadLink.href = uri;
-	downloadLink.download = fileName;
-    
-	document.body.appendChild(downloadLink);
-	downloadLink.click();
-	document.body.removeChild(downloadLink);
-	};
-            
-        my.createCORSRequest = function(method, url) {
-            var xhr = new XMLHttpRequest();
-            if ("withCredentials" in xhr) {
-                xhr.open(method, url, true);
-            } else if (typeof XDomainRequest != "undefined") {
-                xhr = new XDomainRequest();
-                xhr.open(method, url);
-            } else {
-                xhr = null;
-            }
-                
-            return xhr;
-        };
-	    
-	my.requestData = function (url, success, error) {
-	var httpRequest = new XMLHttpRequest();
-	httpRequest.open("GET", url, true);
-		
-	httpRequest.onload = function () {
-                var headers = {};
-                var headersRaw = httpRequest.getAllResponseHeaders().toLowerCase();
-                headersRaw = headersRaw.split(/\r?\n/);
-                UTIL.foreach(headersRaw, function (index, headerRaw) {
-                    var headerParts = headerRaw.split(":");
-                    var haderName =  headerParts[0];
-                    headerParts.splice(1,0);
-                        
-                    headers[haderName] = headerParts.join(":");
-                });
-                    
-		success(JSON.parse(httpRequest.responseText), headers);
-	}
-		
-	httpRequest.onerror = function () {
-                error(JSON.parse(httpRequest.responseText));
-	}
-		
-	httpRequest.withCredentials = false;
-	httpRequest.send();
-	};
-	    
-	return my;
+    my.foreach = function (data, callback) {
+        for (var idxElement = 0; idxElement < data.length; idxElement++) {
+            callback(idxElement, data[idxElement]);
+        }
+    };
+
+    my.parallel = function (actions, finished) {
+        finishedCount = 0;
+        var results = [];
+
+        UTIL.foreach(actions, function (i, action) {
+            action.action(function (result) {
+                results[i] = result;
+                finishedCount++;
+
+                if (finishedCount == actions.length) {
+                    finished(results);
+                }
+            }, action.data);
+        });
+    };
+
+    my.saveFile = function (type, data, fileName) {
+        var uri = 'data:' + type + ';charset=utf-8,' + data;
+
+        var downloadLink = document.createElement("a");
+        downloadLink.href = uri;
+        downloadLink.download = fileName;
+
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    };
+
+    my.createCORSRequest = function (method, url) {
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+            xhr.open(method, url, true);
+        } else if (typeof XDomainRequest != "undefined") {
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+        } else {
+            xhr = null;
+        }
+
+        return xhr;
+    };
+
+    my.requestData = function (url, success, error) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open("GET", url, true);
+
+        httpRequest.onload = function () {
+            var headers = {};
+            var headersRaw = httpRequest.getAllResponseHeaders().toLowerCase();
+            headersRaw = headersRaw.split(/\r?\n/);
+            UTIL.foreach(headersRaw, function (index, headerRaw) {
+                var headerParts = headerRaw.split(":");
+                var haderName = headerParts[0];
+                headerParts.splice(1, 0);
+
+                headers[haderName] = headerParts.join(":");
+            });
+
+            success(JSON.parse(httpRequest.responseText), headers);
+        }
+
+        httpRequest.onerror = function () {
+            error(JSON.parse(httpRequest.responseText));
+        }
+
+        httpRequest.withCredentials = false;
+        httpRequest.send();
+    };
+
+    return my;
 }(UTIL || {}));
 
 var GitBugs = (function (my) {
@@ -138,100 +138,100 @@ var GitBugs = (function (my) {
         ],
     };
 
-	var processHeader = function (fields, prefix) {
-	var line = '';
-    
-	for (var field in fields) {
-		if (Array.isArray(fields[field])) {
-			for (var idxArray = 0; idxArray < 5; idxArray++) {
-			line += processHeader(fields[field][0], prefix + field + "_" + idxArray + "_");
-			line = line.substring(0, line.length - 1);
-			}
-		}
-		else if (typeof (fields[field]) === "object") {
-			line += processHeader(fields[field], prefix + field + "_");
-			line = line.substring(0, line.length - 1);
-		}
-		else {
-			line += prefix + field;
-		}
-    
-		line += ",";
-	}
-    
-	return line;
-	}
-	    
-	var processDataObject = function (fields, object) {
-	var line = '';
-    
-	for (var field in fields) {
-		var value = object === null || typeof (object) === "undefined" ? "" : object[field];
-    
-		if (Array.isArray(fields[field])) {
-		for (var idxArray = 0; idxArray < 5; idxArray++) {
-			var arrayValue = !value || idxArray >= value.length ? null : value[idxArray];
-			line += processDataObject(fields[field][0], arrayValue);
-			line = line.substring(0, line.length - 1);
-		}
-		}
-		else if (typeof (fields[field]) === "object") {
-		line += processDataObject(fields[field], value);
-		line = line.substring(0, line.length - 1);
-		}
-		else if (object && object.hasOwnProperty(field)) {
-		if (typeof (value) === "string") {
-			line += value.replace(/,/g, " ");
-		}
-		else {
-			line += value;
-		}
-		}
-		line += ",";
-	}
-    
-	return line;
-	}
-	    
-	var processData = function (fields, result) {
-	var str = '';
-    
-	for (var i = 0; i < result.length; i++) {
-		var topObject = result[i];
-		var line = processDataObject(fields, topObject);
-    
-		line = line.substring(0, line.length - 1);
-		str += line + "%0A";
-	}
-    
-	return str;
-	}
-	    
-	my.download = function (user, repo, state) {
-	var url = "https://api.github.com/repos/" + user + "/" + repo + "/issues?state=" + state;
-	UTIL.requestData(url, function (response, headers) {
-		var totalPages = /page=([0-9]+)>; rel=\"last/g.exec(headers.link)[1];
+    var processHeader = function (fields, prefix) {
+        var line = '';
 
-		var allActions = [];
-		for (var idxPage = 1; idxPage <= totalPages; idxPage++) {
-		allActions.push({
-			action: function (completed, data) {
-			UTIL.requestData(data, completed);
-			},
-			data: url + "&page=" + idxPage
-		});
-		}
+        for (var field in fields) {
+            if (Array.isArray(fields[field])) {
+                for (var idxArray = 0; idxArray < 5; idxArray++) {
+                    line += processHeader(fields[field][0], prefix + field + "_" + idxArray + "_");
+                    line = line.substring(0, line.length - 1);
+                }
+            }
+            else if (typeof (fields[field]) === "object") {
+                line += processHeader(fields[field], prefix + field + "_");
+                line = line.substring(0, line.length - 1);
+            }
+            else {
+                line += prefix + field;
+            }
 
-		UTIL.parallel(allActions, function (results) {
-		var csvData = processHeader(csvFields, "") + "%0A";
-		for (var result in results) {
-			csvData += processData(csvFields, results[result]);
-		}
+            line += ",";
+        }
 
-		UTIL.saveFile('text/csv', csvData, "gitbugs.csv")
-		});
-	});
-	}
-	    
-	return my;
-}(GitBugs|| {}));
+        return line;
+    }
+
+    var processDataObject = function (fields, object) {
+        var line = '';
+
+        for (var field in fields) {
+            var value = object === null || typeof (object) === "undefined" ? "" : object[field];
+
+            if (Array.isArray(fields[field])) {
+                for (var idxArray = 0; idxArray < 5; idxArray++) {
+                    var arrayValue = !value || idxArray >= value.length ? null : value[idxArray];
+                    line += processDataObject(fields[field][0], arrayValue);
+                    line = line.substring(0, line.length - 1);
+                }
+            }
+            else if (typeof (fields[field]) === "object") {
+                line += processDataObject(fields[field], value);
+                line = line.substring(0, line.length - 1);
+            }
+            else if (object && object.hasOwnProperty(field)) {
+                if (typeof (value) === "string") {
+                    line += value.replace(/,/g, " ");
+                }
+                else {
+                    line += value;
+                }
+            }
+            line += ",";
+        }
+
+        return line;
+    }
+
+    var processData = function (fields, result) {
+        var str = '';
+
+        for (var i = 0; i < result.length; i++) {
+            var topObject = result[i];
+            var line = processDataObject(fields, topObject);
+
+            line = line.substring(0, line.length - 1);
+            str += line + "%0A";
+        }
+
+        return str;
+    }
+
+    my.download = function (user, repo, state) {
+        var url = "https://api.github.com/repos/" + user + "/" + repo + "/issues?state=" + state;
+        UTIL.requestData(url, function (response, headers) {
+            var totalPages = /page=([0-9]+)>; rel=\"last/g.exec(headers.link)[1];
+
+            var allActions = [];
+            for (var idxPage = 1; idxPage <= totalPages; idxPage++) {
+                allActions.push({
+                    action: function (completed, data) {
+                        UTIL.requestData(data, completed);
+                    },
+                    data: url + "&page=" + idxPage
+                });
+            }
+
+            UTIL.parallel(allActions, function (results) {
+                var csvData = processHeader(csvFields, "") + "%0A";
+                for (var result in results) {
+                    csvData += processData(csvFields, results[result]);
+                }
+
+                UTIL.saveFile('text/csv', csvData, "gitbugs.csv")
+            });
+        });
+    }
+
+    return my;
+}(GitBugs || {}));
